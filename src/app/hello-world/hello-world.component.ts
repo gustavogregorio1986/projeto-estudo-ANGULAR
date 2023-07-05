a9f428e052853adfb12ac6b7bcf96512966b835e
guga56 <gustavogregorio086@gmail.com>
2023-07-05 20:36:26 -0300

    projeto em angular para estudo

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent {
+  message = 'Hello, World!';
+
+  fontColor = 'blue';
+  sayHelloId = 1;
+  canClick = true;
+
+  canEdit = false;
+
+  sayMessage() {
+    alert(this.message);
+  }
+
+
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = "I'm read only!";
+    }
+  }
+}
